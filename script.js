7e70aaa3958a0b898a4e366affb27af2433d12c7
Gabriel-VitorS <gabrielvitordasilva@gmail.com>
2021-06-02 22:35:22 -0300

    Mobile

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+function abre(){
+
+	var abreMenu = document.getElementById('menu');
+
+    abreMenu.style.left = '0px';
+
+}
+
+
+
+function fecha(){
+    var fecha = document.getElementById('menu');
+
+    fecha.style.left = '-800px';
+
+}
+
+
+
